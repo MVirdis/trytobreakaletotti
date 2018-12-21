e4d1203bb1948c705c8986f5f4524ee643ae1be1
Zampow <therealalessiozambelli@gmail.com>
2018-12-21 15:15:57 +0100

    Added show and hide function to the form.

---
+++ scripts/stylescripts.js
@@ -0,0 +1,17 @@
+function hide(){
+    var blocco = document.getElementById('prenotazione_incontro');
+    var pulsante_mostra = document.getElementById('prenota');
+    blocco.style.visibility = "hidden";
+    blocco.style.margin = "20px auto";
+    pulsante_mostra.style.visibility = "visible";
+    pulsante_mostra.style.height = "40px";
+}
+
+function show(){
+    var blocco = document.getElementById('prenotazione_incontro');
+    var pulsante_mostra = document.getElementById('prenota');
+    blocco.style.visibility = "visible";
+    blocco.style.margin = "0 auto";
+    pulsante_mostra.style.visibility = "hidden";
+    pulsante_mostra.style.height = "0";
+}
