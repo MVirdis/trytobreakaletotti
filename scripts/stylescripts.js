ab248cf36c9f0c619b0e1b4325dfa43623ddd8da
Mario Virdis <virdis.mario97@gmail.com>
2018-12-21 19:20:19 +0100

    All js variables are now camelCase

--- scripts/stylescripts.js
+++ scripts/stylescripts.js
@@ -1,20 +1,20 @@
 var blocco = document.getElementById('prenotazione_incontro');
-var pulsante_mostra = document.getElementById('prenota');
+var pulsanteMostra = document.getElementById('prenota');
 
-var nome_combattente = document.getElementById('nome_combattente');
+var nomeCombattente = document.getElementById('nome_combattente');
 var nome = document.getElementById('nome');
 var cognome = document.getElementById('cognome');
 var mail = document.getElementById('mail');
 
-var request_fields = [nome_combattente, nome, cognome, mail];
+var requestFields = [nomeCombattente, nome, cognome, mail];
 
 function hide() {
     blocco.style.visibility = "hidden";
     blocco.style.margin = "20px auto";
-    pulsante_mostra.style.visibility = "visible";
-    pulsante_mostra.style.height = "40px";
+    pulsanteMostra.style.visibility = "visible";
+    pulsanteMostra.style.height = "40px";
     blocco.style.height = "40px";
-    request_fields.forEach(function(field) { // Resets all input fields
+    requestFields.forEach(function(field) { // Resets all input fields
         field.value = "";
     });
 }
@@ -22,7 +22,7 @@ function hide() {
 function show() {
     blocco.style.visibility = "visible";
     blocco.style.margin = "0 auto";
-    pulsante_mostra.style.visibility = "hidden";
-    pulsante_mostra.style.height = "0";
+    pulsanteMostra.style.visibility = "hidden";
+    pulsanteMostra.style.height = "0";
     blocco.style.height = "250px";
 }
